8a48756c701f8b60925ebc77743830a777ceb156
alinor007 <alinorabdulgafor@gmail.com>
2026-04-25 17:44:38 +0800

    fix UI

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
   devIndicators: false,
-  // images: {
-  //   unoptimized: false,
-  // },
+ 
 }
 
 export default nextConfig
