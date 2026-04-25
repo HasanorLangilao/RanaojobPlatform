3e2beb7764740a3aa6c3027bc51b2d945b23737b
alinor007 <alinorabdulgafor@gmail.com>
2026-04-26 02:19:38 +0800

    update lib/firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "@/config/firebase";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getDatabase, ref, set, onDisconnect, serverTimestamp } from "firebase/database";
